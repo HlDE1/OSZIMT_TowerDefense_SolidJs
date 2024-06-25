590d568601f7c2e08080629f6ada55157cfeeede
HlDE1 <Natixbox@hotmail.de>
2024-06-25 18:17:57 +0200

    created weapon class

--- src/GameClasses/Player.ts
+++ src/GameClasses/Player.ts
@@ -1,15 +1,17 @@
 import styles from "./Player.module.css";
+import Weapon from "./Weapon";
 
 export default class Player {
   name: string;
   posX: number;
   posY: number;
   player?: HTMLDivElement;
-  //TODO: weapons
-  weapons?: any[];
+  weapons?: Weapon[];
   selectedWeapon?: any;
   currentAngle = 0.25;
+  rotationRate = 0.03;
   area: HTMLDivElement;
+
   constructor(
     name: string,
     posX: number,
@@ -22,11 +24,13 @@ export default class Player {
     this.posY = posY;
     this.player = player;
     this.area = area;
-    // this.spawnWeapon();
-    document.addEventListener("keydown", (event) => this.inputEvents(event), true);
+
+    this.weapons = [new Weapon(10, 30, this.area, this)];
+
+    //TODO: remove event listener
+    document.addEventListener("keydown", (event) => this.inputEvents(event));
   }
 
-  //
   getName() {
     return this.name;
   }
@@ -43,68 +47,18 @@ export default class Player {
     if (!this.player) return;
 
     this.rotate(event);
-    this.attack(event);
+    this?.weapons?.[0].fire(event);
   }
 
   rotate(event: KeyboardEvent) {
     if (!this.player) return;
 
-    const changeRate = 0.03;
-
-    if (event.key === "w") {
-      this.currentAngle -= changeRate;
-    } else if (event.key === "s") {
-      this.currentAngle += changeRate;
+    if (event.key === "w" || event.key === "a") {
+      this.currentAngle -= this.rotationRate;
+    } else if (event.key === "s" || event.key === "d") {
+      this.currentAngle += this.rotationRate;
     }
 
     this.player.style.transform = `rotate(${this.currentAngle}turn)`;
-  }
-
-  spawnWeapon() {
-    if (!this.player) return;
-
-    const weaponElement = document.createElement("div");
-    weaponElement.classList.add(styles.player_weapon);
-    this.area.appendChild(weaponElement);
-
-    const playerRect = this.player?.getBoundingClientRect();
-    const weaponRect = weaponElement?.getBoundingClientRect();
-
-    weaponElement.style.top = `${playerRect.top + playerRect.height / 2 - weaponRect.height / 2}px`;
-    weaponElement.style.left = `${playerRect.left + playerRect.width / 2}px`;
-
-    return weaponElement;
-  }
-
-  attack(event: KeyboardEvent) {
-    if (event.key !== " ") return;
-
-    const weapon = this.spawnWeapon();
-    if (!weapon) return;
-
-    weapon.style.rotate = `${-0.25 + this.currentAngle}turn`;
-    let weaponX_Pos = weapon.getBoundingClientRect().left;
-    let weaponY_Pos = weapon.getBoundingClientRect().top;
-
-    const weaponSpeed = 20;
-    const rotationAngle = -0.25 + this.currentAngle;
-    const radianAngle = rotationAngle * 2 * Math.PI;
-    console.log(radianAngle, Math.tan(radianAngle), weaponSpeed * Math.tan(radianAngle));
-
-    const interval = setInterval(() => {
-      if (weaponX_Pos > this.area.clientWidth) {
-        //console.log("out of bounds");
-        clearInterval(interval);
-        this.area.removeChild(weapon);
-        return;
-      }
-
-      weaponX_Pos += weaponSpeed;
-      weaponY_Pos += Math.tan(radianAngle);
-
-      weapon.style.transform = `translate(${weaponX_Pos}px,${weaponY_Pos}px)`;
-      //weapon.style.left = `${weaponX_Pos}px`;
-      // weapon.style.top = `${weaponY_Pos}px`;
-    }, 100);
   }
 }
--- src/GameClasses/Weapon.ts
+++ src/GameClasses/Weapon.ts
@@ -1,9 +1,68 @@
+import Player from "./Player";
+import styles from "./Weapon.module.css";
+
 export default class Weapon {
   damage: number;
   speed: number;
+  area: HTMLDivElement;
+  player?: Player;
 
-  constructor(damage: number, speed: number) {
+  constructor(damage: number, speed: number, area: HTMLDivElement, player?: Player) {
     this.damage = damage;
     this.speed = speed;
+    this.area = area;
+    this.player = player;
+  }
+
+  createWeapon() {
+    if (!this.player?.player) return;
+
+    const weaponElement = document.createElement("div");
+    weaponElement.classList.add(styles.player_weapon);
+    this.area.appendChild(weaponElement);
+
+    const playerRect = this.player.player.getBoundingClientRect();
+    const weaponRect = weaponElement?.getBoundingClientRect();
+
+    weaponElement.style.top = `${playerRect.top + playerRect.height / 2 - weaponRect.height / 2}px`;
+    weaponElement.style.left = `${playerRect.left + playerRect.width / 2}px`;
+
+    return weaponElement;
+  }
+
+  fire(event: KeyboardEvent) {
+    console.log("fire");
+    if (event.key !== " " || !this.player?.player) return;
+
+    const weapon = this.createWeapon();
+    if (!weapon) return;
+
+    weapon.style.rotate = `${-0.25 + this.player.currentAngle}turn`;
+    const weaponRect = weapon.getBoundingClientRect();
+    let weaponX_Pos = weaponRect.left;
+    let weaponY_Pos = weaponRect.top;
+
+    const weaponSpeed = 30;
+    const rotationAngle = -0.25 + this.player.currentAngle;
+    const radianAngle = rotationAngle * 2 * Math.PI;
+
+    const interval = setInterval(() => {
+      weaponX_Pos += weaponSpeed * Math.cos(radianAngle);
+      weaponY_Pos += weaponSpeed * Math.sin(radianAngle);
+
+      const isX_OutOfBounds = weaponX_Pos > this.area.clientWidth || weaponX_Pos < 0;
+      const isY_OutOfBounds =
+        this.area.clientHeight - weaponSpeed > weaponY_Pos ||
+        weaponY_Pos > this.area.clientHeight * 2;
+
+      if (isX_OutOfBounds || isY_OutOfBounds) {
+        clearInterval(interval);
+        this.area.removeChild(weapon);
+        return;
+      }
+
+      weapon.style.left = `${weaponX_Pos}px`;
+      weapon.style.top = `${weaponY_Pos}px`;
+    }, 100);
   }
 }
